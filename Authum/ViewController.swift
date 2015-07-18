1a255777a2b3d4b23c5cae9e6585e2e9bd32aa64
Andrew Clissold <andrewclissold@gmail.com>
2015-07-18 13:10:28 -0400

    Initial commit

---
+++ Authum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Authum
+//
+//  Created by Andrew Clissold on 7/18/15.
+//  Copyright © 2015 Hoodbluck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
