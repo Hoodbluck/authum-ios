76eed9fd549feb9ad7b22524c5561ceade67d7e9
tolkiana <tolkiana@gmail.com>
2015-07-19 03:14:39 -0400

    Implemented tableView delegation methods

--- Authum/HomeViewController.swift
+++ Authum/HomeViewController.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class HomeViewController: UITableViewController {
     
+    let thirdPartyApps = ["Bananarama", "Chase", "Capital One"]
+    let thirdPartyCellIdentifier = "thirdPartyAppCell"
+
+    override func viewDidLoad() {
+        tableView.backgroundColor = UIColor.whiteColor()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return thirdPartyApps.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("thirdPartyAppCell") as? ThirdPartyAppCell
+     
+        if cell == nil {
+            cell = ThirdPartyAppCell(style: .Default, reuseIdentifier: thirdPartyCellIdentifier)
+        }
+        
+        cell?.appNameLabel.text = thirdPartyApps[indexPath.row]
+        cell?.appIconImage.image = UIImage(named: thirdPartyApps[indexPath.row])
+        
+        return cell!
+    }
 }
