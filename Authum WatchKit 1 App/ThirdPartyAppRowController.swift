5428b3cace50734eca0c1f8a65405477e4bf0ce3
Andrew Clissold <andrewclissold@gmail.com>
2015-07-19 08:28:52 -0400

    Create watch app UI

---
+++ Authum WatchKit 1 App/ThirdPartyAppRowController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdPartyAppRowController.swift
+//  Authum
+//
+//  Created by Andrew Clissold on 7/19/15.
+//  Copyright © 2015 Hoodbluck. All rights reserved.
+//
+
+import WatchKit
+
+class ThirdPartyAppRowController: NSObject {
+
+    @IBOutlet weak var image: WKInterfaceImage!
+    @IBOutlet weak var label: WKInterfaceLabel!
+
+}
--- Authum WatchKit 1 Extension/InterfaceController.swift
+++ Authum WatchKit 1 Extension/InterfaceController.swift
@@ -9,23 +9,22 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var table: WKInterfaceTable!
+
+    let names = ["Bananarama", "Chase", "Capital One"]
+
     override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
+        table.setNumberOfRows(names.count, withRowType: "thirdPartyApp")
 
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+        for i in 0..<table.numberOfRows {
+            guard let thirdPartyAppRowController = table.rowControllerAtIndex(i) as? ThirdPartyAppRowController else {
+                fatalError("could not create ThirdPartyAppRowController")
+            }
+            thirdPartyAppRowController.image.setImageNamed(names[i])
+            thirdPartyAppRowController.label.setText(names[i])
+        }
     }
 
 }
